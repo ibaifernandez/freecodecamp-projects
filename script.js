d175f8950cbaaa19239cac258eb5c97771606424
Ibai Fernández <info@ibaifernandez.com>
2022-12-05 11:26:11 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function showTitle() {
-  let upLeftBox = document.getElementById("up-left-box");
-  const title = document.createElement("h2");
-  h2.innerHTML = '<h2 class="web-name">Charles Bukowski</h2>';
-  upLeftBox.appendChild(h2);
-}
